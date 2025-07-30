7b9b1a2b9da2685c4e50553ebcdffc535f1a38f4
Kunal Dnyaneshwar Kamod <kunalkamod3@gmail.com>
2025-07-30 21:53:45 +0530

    Update twoi.c

--- twoi.c
+++ twoi.c
@@ -7,11 +7,16 @@ int main()
     printf("Enter two integers:\n");
     scanf("%d %d", &a, &b);
 
-    if (a == b) {
+    if (a == b) 
+    {
         printf("Both numbers are equal.\n");
-    } else if (a > b) {
+    } 
+    else if (a > b) 
+    {
         printf("%d is greater than %d.\n", a, b);
-    } else {
+    } 
+    else 
+    {
         printf("%d is greater than %d.\n", b, a);
     }
 
