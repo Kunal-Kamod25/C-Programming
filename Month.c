09c6e6e5f30e7d0b789dd6c718a0db31cc1329ee
kunal-kamod25 <kunalkamod3@gmail.com>
2025-08-26 21:17:16 +0530

    Added more C programs

---
+++ Month.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+	int n=0;
+	
+	printf("Enter A Number=");
+	scanf("%d",&n);
+	
+	switch(n)
+	{
+		case 1:
+			printf("The Month Is January \n");
+			break;
+		case 2:
+			printf("The Month Is Fabruary \n");
+			break;
+		case 3:
+			printf("The Month Is March \n");
+			break;
+		case 4:
+			printf("The Month Is April \n");
+			break;
+		case 5:
+			printf("The Month Is May \n");
+			break;
+		case 6:
+			printf("The Month Is June \n");
+			break;
+		case 7:
+			printf("The Month Is July \n");
+			break;
+		case 8:
+			printf("The Month Is August \n");
+			break;
+		case 9:
+			printf("The Month Is September \n");
+			break;
+		case 10:
+			printf("The Month Is October \n");
+			break;
+		case 11:
+			printf("The Month Is November \n");
+			break;
+		case 12:
+			printf("The Month Is December \n");
+			break;
+		default:
+			printf("Enter Invalid Number.\n");
+	}
+}
+			
+			
---
+++ Palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+        int n=0,ans=0;
+        printf("Enter the number: \n");
+        scanf("%d",&n);
+
+        int n1=n;
+        while(n>0)
+        {
+                ans=(ans*10)+n%10;
+                n=n/10;
+        }
+        printf("ans=%d",ans);
+        if(ans==n1)
+        printf("n=%d is palindrome",n1);
+        else
+        printf("not palindrome");
+        return 0;
+}
