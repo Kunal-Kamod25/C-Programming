efedd8a8708363542a27bece82a311d929e3a412
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-19 21:33:18 +0530

    Deleted obsolete files from Test_1

--- Test_1/Test21b.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a,b;
-    printf("Enter Two Integers: ");
-    scanf("%d %d",&a,&b);
-
-    (a>b)?printf("The Larger Number is:%d",a):printf("The Larger Number is:%d",b);
-}
