574c7f4a3b9ba835044c1577b0c0fcf048d2cf8f
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-20 22:30:19 +0530

    Array

---
+++ Array/Array4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int marks[5]={45, 65, 44, 34, 56};
+    int i;
+    
+    for(i=0;i<5;++i)
+    {
+        printf("Value = %d\n",marks[i]);
+    }
+    return 0;
+}
