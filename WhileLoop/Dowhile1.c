1a149b8c97d113afc3ef4ad7517b786b6b9f4dda
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-24 00:55:30 +0530

    Using do while print no

---
+++ WhileLoop/Dowhile1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    int i=1;
+
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    do
+    {
+        printf("%d\n",i);
+        ++i;
+    } while (i <= n);
+    return 0;
+    
+}
