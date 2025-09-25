100bf8c43805005fcb57359fd9c8aa797102854d
kunal-kamod25 <kunalkamod3@gmail.com>
2025-09-25 21:02:46 +0530

    Get maximum number from list

---
+++ Array/Maximum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    int*A=(int*)malloc(sizeof(int)*n);
+    if(A==NULL)
+    {
+        printf("Memory allocation faild.....\n");
+        exit(-1);
+    }
+    printf("Enter a array: ");
+    for(int i=0; i<n; ++i)
+    {
+        scanf("%d",&A[i]);
+    }
+    int max=A[0];
+    for(int i=1;i<n; ++i)
+        {
+            if(A[i]>max)
+            max=A[i];
+        }
+        printf("Maximum= %d\n",max);
+    free(A);
+    return 0;
+    
+}
