ef7f66c51c9976e04acbc056f7c77f7cc6d3b3ed
Kunal Kamod <kunalkamod3@gmail.com>
2025-09-30 20:44:23 +0530

    Add files via upload
    calculate sum

---
+++ ForLoop/CalSum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    int sum=0;
+    for(int i=5; i<=n; ++i)
+    {
+        sum=sum+i;
+    }
+    printf("The sum is :: %d",sum);
+    return 0;
+}
