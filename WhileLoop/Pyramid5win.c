4c3ff7bcad6b055f7ea445d3ec0b5868bd41c125
Kunal Kamod <kunalkamod3@gmail.com>
2025-10-02 22:05:45 +0530

    Add files via upload

---
+++ WhileLoop/Pyramid5win.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int row, col;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    row = n;
+    while (row >= 1)
+    {
+        col = 1;
+        while (col <= row)
+        {
+            printf("%d", col);
+            col++;
+        }
+        printf("\n");
+        row--;          
+    }
+
+    return 0;
+}
