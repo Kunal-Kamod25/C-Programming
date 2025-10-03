94ced5d27d8ace5fb5e887089ff888a35448f667
Kunal Kamod <kunalkamod3@gmail.com>
2025-10-03 22:41:10 +0530

    Update Usereven.c

--- WhileLoop/Usereven.c
+++ WhileLoop/Usereven.c
@@ -6,7 +6,7 @@ int main()
     
     do
     {
-        printf("Enter a number: ");
+        printf("Enter a Number: ");
         scanf("%d",&n);
         printf("%d\n",n);
 
@@ -16,7 +16,8 @@ int main()
         }
     }
     while(1);
-    printf("Thank you\n");
+    printf("Thank You.\n");
 
     return 0;
+
 }
