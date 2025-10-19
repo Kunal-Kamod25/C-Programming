ade4ea92b4e3e39ae0510a4cb2a87bf838764aa3
Kunal Kamod <kunalkamod3@gmail.com>
2025-10-19 18:45:55 +0530

    Add files via upload

---
+++ All_Codes/Check_No_Pos_Neg_Zero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a no: ");
+    scanf("%d",&n);
+    if(n > 0)
+    {
+        printf("%d is Positive no. \n",n);
+    }
+    else if(n < 0)
+    {
+        printf("%d is Negative no. \n",n);
+    }
+    else
+    {
+        printf("%d is Negative no. \n",n);
+    }
+}
