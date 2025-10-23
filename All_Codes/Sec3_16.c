a7a778e98b58d46dde4ed629d240f676d1fe9214
Kunal Kamod <kunalkamod3@gmail.com>
2025-10-24 00:36:52 +0530

    Add files via upload

---
+++ All_Codes/Sec3_16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a , b;
+    printf("Enter two integers: ");
+    scanf("%d %d",&a , &b);
+
+    if(b != 0)
+    {
+        printf("Quotient = %d \n",a / b);
+        printf("Remainder = %d \n",a % b);
+    }
+    else
+        printf("Division by zero is not allowed \n");
+
+    return 0;
+}
