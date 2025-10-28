7b078de263976340810a23a38ba4d0b22b1be254
Kunal Kamod <kunalkamod3@gmail.com>
2025-10-28 22:00:39 +0530

    Correct variable name and format in AddTwoNT.c
    Fixed typo in variable name and added newline at end of file.

--- All_Codes/AddTwoNT.c
+++ All_Codes/AddTwoNT.c
@@ -5,7 +5,8 @@ int main()
     char Operator='+';
     printf("Enter Two Numbwrs: ");
     scanf("%d %d",&num1 , &num2);
-    result=(Operator='+')?(num1+num2):(num1-num2);
+    result=(operator='+')?(num1+num2):(num1-num2);
     printf("%d",result);
     return 0;
+
 }
