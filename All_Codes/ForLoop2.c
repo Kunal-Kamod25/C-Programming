dbd7693c48891a03fef48dc50f861073e692be17
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-02 23:15:05 +0530

    Fix formatting and add newline at end of file

--- All_Codes/ForLoop2.c
+++ All_Codes/ForLoop2.c
@@ -2,11 +2,12 @@
 int main()
 {
     int i=0, n;
-    printf("Enter the number: ");
+    printf("Enter the Number: ");
     scanf("%d",&n);
 
     for(i=0; i<=n; i++)
     {
         printf("%d \n",i);
     }  
+
 }
