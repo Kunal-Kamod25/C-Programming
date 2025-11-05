7ff45946f8366be1f66c634d5ebe9080216daba1
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-05 23:49:37 +0530

    Add files via upload

---
+++ All_Codes/Sec5_44.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    printf("Enter size of the array: ");
+    scanf("%d",&n);
+
+    int*A=(int *)malloc(sizeof(int)*n);
+    printf("Enter %d elements: ",n);
+    for(int i = 0 ; i < n ; i++)
+        scanf("%d",&A[i]);
+
+    int flag=1;
+    for(int j = 0 ; j < n/2 ; j ++)
+    {
+        if(A[j] != A[n-j-1])
+            flag = 0;
+            break;
+    }
+    if(flag)
+        printf("Array is palindrome\n");
+    else    
+        printf("Array is not a palindrome\n");
+    
+    free(A);
+    return 0;
+}
