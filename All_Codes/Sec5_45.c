31aa7a419bbcb4ea3ed694f37e3fc4526d804142
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-05 23:50:03 +0530

    Add files via upload

---
+++ All_Codes/Sec5_45.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    printf("Enter size of the array: ");
+    scanf("%d",&n);
+
+    int*A=(int *)malloc(sizeof(int)*n);
+    printf("Enter %d elements: ",n);
+    for(int i = 0 ; i < n ; i++)
+        scanf("%d",&A[i]);
+
+    int i , j = 0;
+    for(i = 0 ; i < n ; i++)
+    {
+        if(A[i] != 0)
+            A[j++] = A[i];
+    }
+    while(j<n)
+        A[j++] = 0;
+    
+    printf("Array after moving zeros: ");
+    for(i = 0 ; i < n ; i++)
+        printf("%d ",A[i]);
+
+    free(A);
+    return 0;
+}
