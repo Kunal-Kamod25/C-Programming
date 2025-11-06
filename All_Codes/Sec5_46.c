7f8a81cbc0d675151cd2a5afa9c1f6dd0b26da0b
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-06 11:55:59 +0530

    Add files via upload

---
+++ All_Codes/Sec5_46.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n;
+    printf("Enter size of the array: ");
+    scanf("%d",&n);
+
+    int*A=(int *)malloc(sizeof(int)*n);
+    printf("Enter %d elements: ",n);
+    for(int i = 0 ; i < n ; i++)
+        scanf("%d",&A[i]);
+
+    int evensum = 0 , oddsum = 0;
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(i % 2 == 0)
+            evensum = evensum + A[i];
+        else    
+            oddsum = oddsum + A[i];
+    }
+    printf("Difference = %d\n",evensum , oddsum);
+    free(A);
+    return 0;
+}
