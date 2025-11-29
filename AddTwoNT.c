76f4730ae11adf46e3b05ed9236cfa1a3d5e2a96
Kunal Kamod <kunalkamod3@gmail.com>
2025-11-29 18:39:53 +0530

    Fix typo in prompt for entering numbers

--- AddTwoNT.c
+++ AddTwoNT.c
@@ -3,9 +3,10 @@ int main()
 {
     int num1=0,num2=0,result;
     char Operator='+';
-    printf("Enter Two Numbwrs: ");
+    printf("Enter two numbers: ");
     scanf("%d %d",&num1 , &num2);
     result=(Operator='+')?(num1+num2):(num1-num2);
     printf("%d",result);
     return 0;
+
 }
