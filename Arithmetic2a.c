b12158f1d28c2204e44f5ec43dd7eaf5e9150a88
Kunal Kamod <kunalkamod3@gmail.com>
2026-04-12 19:16:07 +0530

    Improve prompt messages for user input

--- Arithmetic2a.c
+++ Arithmetic2a.c
@@ -2,9 +2,9 @@
 int main() 
 {
     int x, y;
-    printf("Enter Integer x: ");
+    printf("Enter an Integer x: ");
     scanf("%d", &x);
-    printf("Enter Integer y: ");
+    printf("Enter an Integer y: ");
     scanf("%d", &y);
 
     int result = (x * x * x) + 3 * (x * x) + 4 * x - (y * y * y);
