adcfc6d120f147a03793b70d3eb02fcc7b366d5e
Kunal Kamod <kunalkamod3@gmail.com>
2026-04-20 23:35:42 +0530

    Fix formatting and spacing in AddTwoNT.c

--- AddTwoNT.c
+++ AddTwoNT.c
@@ -3,7 +3,7 @@ int main()
 {
     int num1=0,num2=0,result;
     char Operator='+';
-    printf("Enter two numbers: ");
+    printf("Enter two Numbers: ");
     scanf("%d %d",&num1 , &num2);
     result=(Operator='+')?(num1+num2):(num1-num2);
     printf("%d",result);
