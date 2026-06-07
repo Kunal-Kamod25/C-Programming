4a36cf3db7a4272827ee07369c031c080297569b
Kunal Kamod <kunalkamod3@gmail.com>
2026-06-07 20:06:43 +0530

    Fix typo in prompt message for user input

--- ForLoop/UNo1.c
+++ ForLoop/UNo1.c
@@ -4,7 +4,7 @@ int main()
     int n=0;
     int i=0;
 
-    printf("Enter how many number you want to print: ");
+    printf("Enter how many number you want to Print: ");
     scanf("%d",&n);
 
     while(i<=n)
